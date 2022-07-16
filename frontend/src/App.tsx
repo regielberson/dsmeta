9c5202e8f8ab3271d89580f002b5bd8717dc82d8
Regielberson Alexandre Pereira Lirio <rlirio@supriservice.local>
2022-07-16 17:51:41 -0300

    firts component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
-
+import NotificationButton from './components/NotificationButton'
 
 function App() {
 
+
   return(
 
-    <h1> Olá Burro! </h1>
+    <NotificationButton/>   
   )
+
+
+  
 }
 
 export default App
